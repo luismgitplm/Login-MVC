ae18b79021e0804b55cf225e12dc2abf5cf55120
luismgitplm <lrodnar@g.educaand.es>
2025-12-19 20:47:06 +0100

    .

--- Views/dashboard.php
+++ Views/dashboard.php
@@ -1,6 +1,20 @@
 <?php
 // Control de acceso no autorizado mediante URL
 
+
+// Si no hay una sesión activa o el token CSRF no es válido, redirigir al login
+if (!isset($_SESSION['idusuario']) || !isset($_SESSION['csrf_token'])) {
+    header('Location: /Login-MVC/index.php?action=login&error=Debes iniciar sesión para continuar');
+    exit();
+}
+
+// Verificar el CSRF token (si es necesario) antes de permitir acceso
+if (isset($_GET['csrf_token']) && $_GET['csrf_token'] !== $_SESSION['csrf_token']) {
+    header('Location: /Login-MVC/index.php?action=login&error=Debes iniciar sesión para continuar');
+    exit();
+}
+
+
 ?>
 <html lang="es">
 
